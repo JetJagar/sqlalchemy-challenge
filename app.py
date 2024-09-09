6c0d5ac3d24715149f0a4f91df63017626b27f8d
JetJagar <ce.mini1991@yahoo.com>
2024-09-09 17:37:26 -0500

    closing vsc

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import flask
 print(flask.__version__)
 import sqlalchemy
 print(sqlalchemy.__version__)
+import datetime as dt
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
@@ -14,26 +15,50 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+measurements = Base.classes.measurement
+stations = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+# home page with all available routes to view api data
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return(
+        f"Available Routes:<br/>"
+        f"/api/v1.0/percipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start><end>"
+    )
+
+# setting up the route for the 'percipitation' api
+@app.route("/api/v1.0/percipitation")
+def percipitation():
+    # Starting from the most recent data point in the database. 
+    most_recent_date = session.query(measurements.date).order_by(measurements.date.desc()).first()[0]
+
+    # Calculate the date one year from the last date in data set.
+    one_year_ago = dt.datetime.strptime(most_recent_date, '%Y-%m-%d') - dt.timedelta(days=365)
+
+    # Perform a query to retrieve the data and precipitation scores
+    results = session.query(measurements.date, measurements.prcp).filter(measurements.date >= one_year_ago).all()
